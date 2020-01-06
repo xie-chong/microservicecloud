27ce394930b9d91cf0535a666d942e32286870fa
xie-chong <xiechongyn@163.com>
2020-01-06 23:08:34 +0800

    init microservicecloud-eureka-7001 eureka服务注册中心Module

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -3,12 +3,13 @@ package com.atguigu.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.atguigu.springcloud.dao")
+@EnableEurekaClient
 public class DeptProvider8001_App {
     public static void main(String[] args) {
-
-        SpringApplication.run(DeptProvider8001_App.class,args);
+        SpringApplication.run(DeptProvider8001_App.class, args);
     }
 }
