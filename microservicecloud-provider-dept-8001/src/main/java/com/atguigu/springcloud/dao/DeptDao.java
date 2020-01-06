ab820c5ca649937aa20e59eb7faaa362a3dace37
xie-chong <xiechongyn@163.com>
2020-01-06 16:21:06 +0800

    IDEA完美解决 Could not autowire. No beans of 'xxx' type found.报错 把 @Mapper 换成 @Repository

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/dao/DeptDao.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/dao/DeptDao.java
@@ -2,10 +2,11 @@ package com.atguigu.springcloud.dao;
 
 import com.atguigu.springcloud.entities.Dept;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Mapper
+@Repository
 public interface DeptDao {
     public boolean addDept(Dept dept);
 
