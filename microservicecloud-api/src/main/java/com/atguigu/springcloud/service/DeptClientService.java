7b609ed1c9ccf2e86ffd785c519b3a6cdc7f5fd1
xie-chong <xiechongyn@163.com>
2020-01-09 22:59:01 +0800

    init Feign Client

---
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientService.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.Dept;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+public interface DeptClientService {
+
+    @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+    boolean add(Dept dept);
+
+    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+    Dept get(Long id);
+
+    @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+    List<Dept> list();
+}
