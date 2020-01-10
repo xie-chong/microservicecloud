cac60a5d7b16de49454584e663230f1dd73d792f
xie-chong <xiechongyn@163.com>
2020-01-10 20:55:17 +0800

    Commit Feign Hystrix 服务降级

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientService.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientService.java
@@ -2,19 +2,25 @@ package com.atguigu.springcloud.service;
 
 import com.atguigu.springcloud.entities.Dept;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+/**
+ * @Description: 修改microservicecloud-api工程，根据已经有的DeptClientService接口
+ * 新建一个实现了FallbackFactory接口的类DeptClientServiceFallbackFactory
+ */
+//@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+@FeignClient(value = "microservicecloud-dept", fallbackFactory = DeptClientServiceFallbackFactory.class)
 public interface DeptClientService {
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     boolean add(Dept dept);
 
     @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
-    Dept get(Long id);
+    Dept get(@PathVariable("id") Long id);
 
     @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
     List<Dept> list();
